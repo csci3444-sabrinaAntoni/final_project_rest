c7beca28927d0c4c2ac7cdae00c96f3a8ee89af4
santoni13 <santoni@student.fdu.edu>
2017-12-26 10:12:04 -0500

    inital commit

---
+++ src/rest/route/teacherRestRouter.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var TeacherModel = require('../model/teacherModel');
+var teacherRestController = require('../controller/teacherRestController')(TeacherModel); // NOTE ilker injecting above imported TeacherModel into rest controller via its constructor function
+
+var teacherRestRouter = express.Router();
+
+teacherRestRouter.route('') // "/teachers" - the root url is defined by user of this router, main_restServer as "/teachers"
+    .get(teacherRestController.find)
+    .post(teacherRestController.save)
+    .delete(teacherRestController.findByIdInBodyThenRemove);
+
+teacherRestRouter.route('/:id') // "teachers/:id"
+    .get(teacherRestController.findById)
+    .put(teacherRestController.findByIdUpdateFullyThenSave)
+    .patch(teacherRestController.findByIdUpdatePartiallyThenSave)
+    .delete(teacherRestController.findByIdThenRemove);
+
+teacherRestRouter.route('/echo/:msg') // "teachers/echo/:msg"
+    .get(teacherRestController.echoMsg)
+
+module.exports = teacherRestRouter;
